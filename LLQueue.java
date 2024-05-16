ae03d34459a27f17548bea7cd2763dbcad63ea33
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-05-15 21:30:18 -0700

    meow

--- LLQueue.java
+++ LLQueue.java
@@ -1,7 +1,5 @@
-/*
- * This class implements a queue with linked list
- * Author: Meng Yang
- * Date: Fall 2018
+/**
+ * Quinn Epstein
  */
 
 public class LLQueue {
@@ -13,49 +11,74 @@ public class LLQueue {
 
         // Constructor with no parameters for inner class
         public Node() {
-           
+            this.data = null;
+            this.next = null;
         }
 
         // Parametrized constructor for inner class
         public Node(Object newData, Node nextLink) {
-            // to do: Data part of Node is an Object
-            // to do: Link to next node is a type Node
+            data = newData;
+            next = nextLink;
         }
     }
-    
+
     private Node front;
     private Node back;
 
     public LLQueue() {
-     // to do
+        front = null;
+        back = null;
     }
-    
+
     //offer(enqueue) adds the object at the back of the queue
     public void offer(Object o) {
-     // to do
+        Node newNode = new Node(o, null);
+        if (isEmpty()) {
+            front = newNode;
+        } else {
+            back.next = newNode;
+        }
+        back = newNode;
     }
-    
-    //poll(dequeue): retrieves and removes the head of this queue, 
+
+    //poll(dequeue): retrieves and removes the head of this queue,
     //or returns null if this queue is empty.
     public Object poll() {
-     // to do
+        if (isEmpty()) {
+            return null;
+        }
+        Object data = front.data;
+        front = front.next;
+        if (front == null) {
+            back = null;
+        }
+        return data;
     }
-    
+
     // Returns the size of linked list by traversing the list
     public int size() {
-        // to do
+        int count = 0;
+        Node current = front;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
     }
-    //peek: Retrieves, but does not remove, the head of this queue, 
+    //peek: Retrieves, but does not remove, the head of this queue,
     //or returns null if this queue is empty.
     public Object peek() {
-        // to do
-    } 
-    
+        if (isEmpty()) {
+            return null;
+        }
+        return front.data;
+    }
+
     //
     public boolean isEmpty() {
-        // to do
-    } 
-    
+        return front == null;
+    }
+
     // For two lists to be equal they must contain the same data items in
     // the same order. The equals method of T is used to compare data items.
     public boolean equals(Object otherObject) {
@@ -79,10 +102,10 @@ public class LLQueue {
             return true; // objects are the same
         }
     }
-    
+
     // There is no need to modify the driver
     public static void main(String[] args) {
-     // input data for testing
+        // input data for testing
         String target = "Somethings!";
         String palindrome = "a man a plan canal panama";
 
@@ -99,10 +122,10 @@ public class LLQueue {
         list.offer(object2);
         list.offer(object3);
         list.offer(object4);
-        
+
         // make sure all are added
         System.out.println("My list has " + list.size() + " nodes.");
-        
+
         //testing equals
         LLQueue list2 = new LLQueue();
         // add 4 objects to the new linked list
@@ -112,7 +135,7 @@ public class LLQueue {
         list2.offer(object4);//s
         boolean isEqual2 = list.equals(list2);
         System.out.println("list2 is equal to list1? " + isEqual2);
-        
+
         // add 4 objects to our linked list in a different order
         LLQueue list3 = new LLQueue();
         list3.offer(object3);//m
@@ -121,15 +144,15 @@ public class LLQueue {
         list3.offer(object4);//s
         boolean isEqual3 = list.equals(list3);
         System.out.println("list3 is equal to list1? " + isEqual3);
-        
+
         // testing isEmpty() and poll()
         while(!list.isEmpty()) {
             Object temp = list.poll();
             System.out.println("Polling " + temp);
         }
-       
+
     }
 
-   
+
 
 }
--- UsingStacksSuitorsLab.java
+++ UsingStacksSuitorsLab.java
@@ -1,122 +1,160 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-/* CSSSKL 162
- * 
+/** CSSSKL 162
+ *
  * UsingStacksSuitorsLab
- * 
- * This class is mostly a driver for playing with Strings as palindromes, 
+ *
+ * This class is mostly a driver for playing with Strings as palindromes,
  * both iteratively and recursively.  The UsingStacksSuitorsLab class itself is
  * a runnable object, so it can be passed to a thread in our Queue demo
- * 
- * 
+ *Quinn Epstein
  */
 
 public class UsingStacksSuitorsLab implements Runnable {
-	private static int threadCount = 0;
-	private String name;
-	
-	public UsingStacksSuitorsLab() {
-		name = "#" + threadCount++ + "Thread";
-	}
-	
-	public static void main(String[] args) {
-		String s1 = "food";		    //not a palindrome
-		String s2 = "racecar";      //a palindrome
-			
-		System.out.println("String1 is \"" + s1 + "\"");
-		System.out.println("String2 is \"" + s2 + "\"");
-		
-	/*	System.out.println(s1 + " reversed is: ");
+    private static int threadCount = 0;
+    private String name;
+
+    public UsingStacksSuitorsLab() {
+        name = "#" + threadCount++ + "Thread";
+    }
+
+    public static void main(String[] args) {
+        String s1 = "food";		    //not a palindrome
+        String s2 = "racecar";      //a palindrome
+
+        System.out.println("String1 is \"" + s1 + "\"");
+        System.out.println("String2 is \"" + s2 + "\"");
+
+		System.out.println(s1 + " reversed is: ");
 		printReverse(s1);
 		System.out.println(s2 + " reversed is: ");
 		printReverse(s2);
-		
+
 	    recPrintReverse(s1);
 		System.out.println();
 		recPrintReverse(s2);
 		System.out.println();
-		
+
 		System.out.println(s1 + " is a palindrome: " + isPalindrome(s1));
 		System.out.println(s2 + " is a palindrome: " + isPalindrome(s2));
-		
+
 		System.out.println(s1 + " is a palindrome(recursively): " + isPalindromeRec(s1));
-		System.out.println(s2 + " is a palindrome(recursively): " + isPalindromeRec(s2));	
-		
+		System.out.println(s2 + " is a palindrome(recursively): " + isPalindromeRec(s2));
+
 		System.out.println("Did we build a Queue of Threads and start them? " + buildThreadQueue());
-		
+
 		int n = 6;
 		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
-		
+
 		n = 10;
-		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));*/
-	}
-		
-	
-	
-	
-	public static void printReverse(String target) {
-		//todo: use a stack
-	}
-	
-	public static void recPrintReverse(String target) {
-		//todo
-	}
-	
-	public static boolean isPalindrome(String input) {
-		//todo: use a stack
-	}
-
-	public static boolean isPalindromeRec(String sentence)	{
-	  	//todo
-	}
-	
-	public static int findPlaceToStand(int numSuitors) {
-		//todo
-		return -1;
-	}	
+		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
+    }
 
 
-	public static boolean buildThreadQueue() {	//returns true upon success
-		Queue<Thread> q = new LinkedList<Thread>(); 
-		
-		//when our program starts up, it might create multiple threads to use
-		q.enqueue( new Thread(new UsingStacksSuitorsLab()));
-		q.enqueue( new Thread(new UsingStacksSuitorsLab()));
-		q.enqueue( new Thread(new UsingStacksSuitorsLab()));
-		
-		System.out.println("Initial Thread order:");
-		q.toString();  
-		
-		//We need to iterate over our pool of threads and call start() on each one
-		//Make a loop that dequeues a thread, calls start on it, and //enqueues it again
-		//to do:
-		//current = get a thread
-		//current.start();
-		//put the thread back
-		
-		System.out.println("Thread order after start()ing:");
-		q.toString();  
-		
-		return true;  //on successful start
-	}
-	
-	
-	/*
-	 * No need to edit anything below here, 
-	 * unless you'd like to change the 
-	 * behaviour of each thread in the thread pool above
-	 */
-	
-	@Override
-	public void run() {
-		for(int i = 0; i < 1000; i++) {
-			System.out.println(name + ": " + i + "th iteration");
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				//do nothing here
-			}
-		}
-	}
+
+
+    public static void printReverse(String target) {
+        //todo: use a stack
+        LLStack stack = new LLStack();
+        for (int i = 0; i < target.length(); i++) {
+            stack.addToStart(target.charAt(i));
+        }
+        System.out.println(target + " reversed is: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.deleteHead());
+        }
+        System.out.println();
+    }
+
+    public static void recPrintReverse(String target) {
+        //todo
+        if (target.length() == 0) {
+            return;
+        }
+        recPrintReverse(target.substring(1));
+        System.out.print(target.charAt(0));
+    }
+
+    public static boolean isPalindrome(String input) {
+        //todo: use a stack
+        LLStack stack = new LLStack();
+        for (int i = 0; i < input.length(); i++) {
+            stack.addToStart(input.charAt(i));
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (!stack.deleteHead().equals(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindromeRec(String sentence)	{
+        //todo
+        if (sentence.length() <= 1) {
+            return true;
+        }
+        if (sentence.charAt(0) != sentence.charAt(sentence.length() - 1)) {
+            return false;
+        }
+        return isPalindromeRec(sentence.substring(1, sentence.length() - 1));
+    }
+
+    public static Object findPlaceToStand(int numSuitors) {
+        //todo
+        LLQueue queue = new LLQueue();
+        for (int i = 1; i <= numSuitors; i++) {
+            queue.offer(i);
+        }
+        while (queue.size() > 1) {
+            queue.offer(queue.poll());
+            queue.poll();
+        }
+        return queue.poll();
+    }
+
+
+    public static boolean buildThreadQueue() {	//returns true upon success
+        Queue<Thread> q = new LinkedList<Thread>();
+
+        //when our program starts up, it might create multiple threads to use
+        q.offer( new Thread(new UsingStacksSuitorsLab()));
+        q.offer( new Thread(new UsingStacksSuitorsLab()));
+        q.offer( new Thread(new UsingStacksSuitorsLab()));
+
+        System.out.println("Initial Thread order:");
+        q.toString();
+
+        //We need to iterate over our pool of threads and call start() on each one
+        //Make a loop that dequeues a thread, calls start on it, and //enqueues it again
+        //to do:
+        //current = get a thread
+        //current.start();
+        //put the thread back
+
+        System.out.println("Thread order after start()ing:");
+        q.toString();
+
+        return true;  //on successful start
+    }
+
+
+    /*
+     * No need to edit anything below here,
+     * unless you'd like to change the
+     * behaviour of each thread in the thread pool above
+     */
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++) {
+            System.out.println(name + ": " + i + "th iteration");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                //do nothing here
+            }
+        }
+    }
 }
